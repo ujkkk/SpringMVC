e1554b822c69640ff858ae5f89b12ecda591af1b
UJeans <jean207@naver.com>
2024-06-19 18:21:14 +0900

    http 요청 데이터 - API 메시지 바디 - 단순 텍스트

---
+++ src/main/java/hello/servlet/basic/request/RequestBodyStringServlet.java
@@ -0,0 +1,28 @@
+package hello.servlet.basic.request;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletInputStream;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.util.StreamUtils;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+@WebServlet(name ="requestBodyStringServlet", urlPatterns = "/request-body-string")
+public class RequestBodyStringServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException, IOException {
+        // 바디의 데이터를 읽기 위해 inputStream을 사용
+        ServletInputStream inputStream = request.getInputStream();
+        // 바이트 정보를 문자열로 바꿔줌
+        String messageBody = StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8);
+        System.out.println("messageBody = " + messageBody);
+        response.getWriter().write("ok");
+
+
+    }
+}
